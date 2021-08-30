bb3392e0c0c80b158fbf11776336a0280b18e7ec
Ming Quan <chua_ming_quan@live.com>
2021-08-31 00:24:32 +0800

    fixed social css

--- src/App.js
+++ src/App.js
@@ -38,6 +38,13 @@ const useStyles = makeStyles(theme => ({
     // outline: "2px dashed blue",
     padding: "3rem",
   },
+  headerSection: {
+    maxWidth: "900px",
+    width: "100%", //prevent child width from shrinking
+    // outline: "2px dashed blue",
+    padding: "3rem",
+    paddingBottom: "150px",
+  },
   divCenter: {
     display:"flex",
     justifyContent: "center"
@@ -55,6 +62,20 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: "#FFFFFF",
     boxShadow: "none",
     color: "#000000",
+  },
+  socials: {
+    display:"flex",
+    justifyContent:"space-between",
+    alignSelf: "center",
+    width: "100px",
+    // outline: "2px dashed blue",
+  },
+  largeIcon: {
+    transform: "scale(2.0)",
+  },
+  outline: {
+    // outline: "2px dashed blue",
+    // maxWidth: "500px"
   },
   myCustomButton: {
     transition: "0.3s",
